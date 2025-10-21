58bff90534d8be579433e85bc75166c0682686e9
CodeKei02 <keicode.dev02@gmail.com>
2025-10-21 15:59:25 -0400

    feat: implement current path

--- src/components/react/NavBar.tsx
+++ src/components/react/NavBar.tsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import navLinks from "../../constants/links/nav-links";
 import { motion } from "motion/react";
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const currentPath = window.location.pathname;
+  const isActive = (item: { href: string }) => currentPath === item.href;
   return (
     <nav className="w-[95%] m-auto relative after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -28,20 +26,21 @@ export const Navbar = () => {
             {/* Desktop navigation: only md and up */}
 
             <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4 ">
-              {navLinks.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  aria-current={item.current ? "page" : undefined}
-                  className={`text-[var(--neutral-900)] font-medium ${
-                    item.current
-                      ? "md:d-inline-block md:border-none md:bg-[var(--neutral-100)] md:after:block md:after:h-[3px] md:after:rounded-[2px] md:after:bg-[var(--orange-500)] md:after:content-['']"
-                      : ""
-                  }`}
-                >
-                  {item.name}
-                </a>
-              ))}
+              {navLinks.map((item) => {
+                return (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={`text-[var(--neutral-900)] font-medium ${
+                      isActive(item)
+                        ? "md:d-inline-block md:border-none md:bg-[var(--neutral-100)] md:after:block md:after:h-[3px] md:after:rounded-[2px] md:after:bg-[var(--orange-500)] md:after:content-['']"
+                        : ""
+                    }`}
+                  >
+                    {item.name}
+                  </a>
+                );
+              })}
             </div>
             <a href="/recipes" className="hidden md:block ml-4 btn text-none">
               Browse Recipes
@@ -57,22 +56,22 @@ export const Navbar = () => {
           animate={{ opacity: 1, height: "auto" }}
           transition={{ duration: 0.3 }}
         >
-          <div className="space-y-1 px-2 pt-2 pb-3">
-            {navLinks.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                aria-current={item.current ? "page" : undefined}
-                className={classNames(
-                  item.current
-                    ? "bg-[var(--neutral-600)] text-white"
-                    : "text-black hover:bg-white/5",
-                  "block rounded-md px-3 py-2 text-base font-medium"
-                )}
-              >
-                {item.name}
-              </a>
-            ))}
+          <div className="flex flex-col justify-center items-center space-y-1 px-2 pt-2 pb-3">
+            {navLinks.map((item) => {
+              return (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={`w-full block rounded-md px-3 py-2 text-base font-medium ${
+                    isActive(item)
+                      ? "bg-[var(--neutral-600)] text-white"
+                      : "text-black hover:bg-white/5"
+                  }`}
+                >
+                  {item.name}
+                </a>
+              );
+            })}
             <button className="w-full btn">Browse recipes</button>
           </div>
         </motion.div>
