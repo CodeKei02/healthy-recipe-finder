bac3fa75c3d3c30114f3c35af4106c77460b19f8
CodeKei02 <keicode.dev02@gmail.com>
2025-10-30 16:38:18 -0400

    fix(images): fixing images path

--- src/constants/home/section-home.ts
+++ src/constants/home/section-home.ts
@@ -1,17 +1,17 @@
 const sectionHome = [
   {
-    img: "/public/icon-whole-food-recipes.svg",
+    img: "/icon-whole-food-recipes.svg",
     title: "Whole-food recipes",
     description: "Each dish uses everyday, unprocessed ingredients.",
   },
   {
-    img: "/public/icon-minimum-fuss.svg",
+    img: "/icon-minimum-fuss.svg",
     title: "Minimum fuss",
     description:
       "All recipes are designed to make eating healthy quick and easy.",
   },
   {
-    img: "/public/icon-search-in-seconds.svg",
+    img: "/icon-search-in-seconds.svg",
     title: "Search in seconds",
     description:
       "Filter by name or ingredient and jump straight to the recipe you need.",
